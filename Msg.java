997f76ea0af59d57e57bca56aab7102af6714e3d
root <root@ip-172-31-30-80.ec2.internal>
2026-02-27 07:00:23 +0000

    my new code

--- Msg.java
+++ Msg.java
@@ -1,5 +1,11 @@
-public class Msg {
-	public static void main(String[] args) {
-		System.out.println("today is 27th february 2026");
-	}
+public class VariablesExample {
+    public static void main(String[] args) {
+        int age = 20;
+        String name = "Rahul";
+        double salary = 25000.50;
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Salary: " + salary);
+    }
 }
+
