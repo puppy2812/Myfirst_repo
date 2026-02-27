b544eac352e0bf77379a558822406ac96a199c42
puppy2812 <rajapanthulajagadeeshwari@gmail.com>
2026-02-27 12:19:50 +0530

    Update Msg.java

--- Msg.java
+++ Msg.java
@@ -1,5 +1,5 @@
-public class Msg{
-	public static void main(String[] args){
-		System.out.println("today is 27th february");
+public class Msg {
+	public static void main(String[] args) {
+		System.out.println("today is 27th february 2026");
 	}
 }
